ab75ee6cf1c5f587c27f48a0e385ca1982fb58d4
himanshi155 <90348232+himanshiagra@users.noreply.github.com>
2026-05-24 20:25:53 +0530

    Update YouTube URLs and add fifth live stream

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 import streamlit as st
 import streamlink
 
+
 st.set_page_config(layout="wide")
+
 
 st.title("Live YouTube Multi Stream Dashboard")
 
+
 youtube_urls = [
-    "https://www.youtube.com/watch?v=NphMcnU8ymU&pp=ygUQZGF0YSB3YXJlaG91c2luZw%3D%3D",
-    "https://www.youtube.com/watch?v=s0LLVQeMmtU&pp=ygUJbGl2ZSBuZXdz0gcJCQQLAYcqIYzv",
-    "http://youtube.com/watch?v=J326LIUrZM8&list=PL9ooVrP1hQOEDSc5QEbI8WYVV_EbWKJwX",
-    "https://www.youtube.com/watch?v=UiTvqSd52ak&list=PLTsNSGeIpGnGP8A74Ie1PgqHhewsqD3fv"
+    "https://www.youtube.com/watch?v=NphMcnU8ymU",
+    "https://www.youtube.com/watch?v=s0LLVQeMmtU",
+    "https://www.youtube.com/watch?v=J326LIUrZM8",
+    "https://www.youtube.com/watch?v=UiTvqSd52ak",
+    "https://www.youtube.com/watch?v=jfKfPfyJRdk"
 ]
 
+
 def get_stream_url(youtube_url):
+
     try:
         streams = streamlink.streams(youtube_url)
 
@@ -25,39 +31,79 @@ def get_stream_url(youtube_url):
         st.error(f"Error loading stream: {e}")
         return None
 
+
 col1, col2 = st.columns(2)
 
-
+# Stream 1
 with col1:
+
     st.subheader("Live Stream 1")
+
     stream_url = get_stream_url(youtube_urls[0])
 
     if stream_url:
         st.video(stream_url)
 
+    else:
+        st.warning("Unable to load stream")
 
+
+# Stream 2
 with col2:
+
     st.subheader("Live Stream 2")
+
     stream_url = get_stream_url(youtube_urls[1])
 
     if stream_url:
         st.video(stream_url)
 
+    else:
+        st.warning("Unable to load stream")
+
+
 
 col3, col4 = st.columns(2)
 
-
+# Stream 3
 with col3:
+
     st.subheader("Live Stream 3")
+
     stream_url = get_stream_url(youtube_urls[2])
 
     if stream_url:
         st.video(stream_url)
 
+    else:
+        st.warning("Unable to load stream")
 
+
+# Stream 4
 with col4:
+
     st.subheader("Live Stream 4")
+
     stream_url = get_stream_url(youtube_urls[3])
 
     if stream_url:
         st.video(stream_url)
+
+    else:
+        st.warning("Unable to load stream")
+
+
+
+
+# Stream 5
+with col5:
+
+    st.subheader("Live Stream 5")
+
+    stream_url = get_stream_url(youtube_urls[4])
+
+    if stream_url:
+        st.video(stream_url)
+
+    else:
+        st.warning("Unable to load stream")
